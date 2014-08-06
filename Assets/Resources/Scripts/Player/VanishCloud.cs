4e88ee4f7e233b4c2df2e913bf24fcd24b09b027
Josh Maggard <jmaggard62@gmail.com>
2014-08-06 17:25:53 -0500

    Vanish particles Closes #52

--- Assets/Resources/Scripts/Player/VanishCloud.cs
+++ Assets/Resources/Scripts/Player/VanishCloud.cs
@@ -2,26 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public class VanishCloud : FContainer
 {
     FAnimatedSprite cloudSprite;
-
+    float particleXSpeed = 30;
+    float particleYSpeed = 30;
+    int numParticles = 20;
+    bool hasAddedParticles = false;
     public VanishCloud()
     {
         cloudSprite = new FAnimatedSprite("vanishCloud");
-        cloudSprite.addAnimation(new FAnimation("disappear", new int[] { 0, 1, 2, 3 }, 100, false));
+        cloudSprite.addAnimation(new FAnimation("disappear", new int[] { 1, 2, 3, 4 }, 100, false));
         this.AddChild(cloudSprite);
+
     }
 
+    float particleDist = 15;
     public override void HandleAddedToContainer(FContainer container)
     {
         Futile.instance.SignalUpdate += Update;
         base.HandleAddedToContainer(container);
+        
     }
 
     private void Update()
     {
+        if (!hasAddedParticles)
+        {
+            for (int x = 0; x < numParticles; x++)
+            {
+                VanishParticle particle = VanishParticle.getParticle();
+                float angle = (RXRandom.Float() * Mathf.PI * 2);
+                Vector2 pos = this.GetPosition() + new Vector2(Mathf.Cos(angle) * particleDist, Mathf.Sin(angle) * particleDist);
+                particle.activate(pos, new Vector2(RXRandom.Float() * particleXSpeed * 2 - particleXSpeed, RXRandom.Float() * particleYSpeed * 2 - particleYSpeed), Vector2.zero, 360);
+                this.container.AddChild(particle);
+            }
+            hasAddedParticles = true;
+        }
         if (cloudSprite.IsStopped)
         {
             this.RemoveFromContainer();
---
+++ Assets/Resources/Scripts/Player/VanishParticle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class VanishParticle : FAnimatedSprite
+{
+    const int MAX_PARTICLES = 100;
+    public static VanishParticle getParticle()
+    {
+        if (particleList == null)
+            particleList = new VanishParticle[MAX_PARTICLES];
+        for (int x = 0; x < particleList.Length; x++)
+        {
+            if (particleList[x] == null)
+            {
+                VanishParticle p = new VanishParticle();
+                particleList[x] = p;
+                return p;
+            }
+            else if (!particleList[x].isActive)
+            {
+                return particleList[x];
+            }
+        }
+        return particleList[RXRandom.Int(MAX_PARTICLES)];
+        
+    }
+    private static VanishParticle[] particleList;
+    Vector2 vel;
+    float rot;
+    Vector2 accel;
+    int animRandom = 180;
+    int animBaseSpeed = 100;
+    public bool isActive = false;
+    private VanishParticle()
+        : base("vanishParticle0" + (RXRandom.Int(3) + 1))
+    {
+        this.isVisible = false;
+        this.addAnimation(new FAnimation("active", new int[] { 1, 2, 3, 4 }, animBaseSpeed + (int)(RXRandom.Float() * animRandom), false)); 
+    }
+
+    public void activate(Vector2 pos, Vector2 vel, Vector2 accel, float rot)
+    {
+        this.isVisible = true;
+        this.isActive = true;
+        this.vel = vel;
+        this.SetPosition(pos);
+        this.rot = rot;
+        this.accel = accel;
+        this.play("active", true);
+    }
+
+    public override void Update()
+    {
+        this.x += vel.x * Time.deltaTime;
+        this.y += vel.y * Time.deltaTime;
+        vel += accel * Time.deltaTime;
+
+        this.rotation += rot * Time.deltaTime;
+
+        if (this.IsStopped)
+        {
+            this.RemoveFromContainer();
+            this.isActive = false;
+        }
+
+        base.Update();
+    }
+}
