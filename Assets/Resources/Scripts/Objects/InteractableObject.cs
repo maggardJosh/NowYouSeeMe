1eabbd8d407c2e83dc5b13aeedd258b5eb6852cf
Josh Maggard <jmaggard62@gmail.com>
2014-08-06 21:42:18 -0500

    Moved graphics

--- Assets/Resources/Scripts/Objects/InteractableObject.cs
+++ Assets/Resources/Scripts/Objects/InteractableObject.cs
@@ -10,7 +10,7 @@ public class InteractableObject : FContainer
     protected float Y_INTERACT_DIST = 10;
     const float MIN_INTERACT_DIST = 20;
     const float MIN_INTERACT_DIST_SQR = MIN_INTERACT_DIST * MIN_INTERACT_DIST;
-    const float INDICATOR_Y = 10;
+    const float INDICATOR_Y = 20;
     const float INDICATOR_BOUNCE = 5;
     protected bool interactable = true;
     public InteractableObject()
@@ -20,6 +20,7 @@ public class InteractableObject : FContainer
         interactInd.isVisible = false;
     }
 
+    public float indDisp { get; set; }
     public bool checkInteractDist(Player p)
     {
         if (!interactable)
@@ -32,10 +33,11 @@ public class InteractableObject : FContainer
             if (!interactInd.isVisible)
             {
                 interactInd.isVisible = true;
-                interactInd.y =INDICATOR_Y;
-                Go.to(interactInd, 1.0f, new TweenConfig().floatProp("y", INDICATOR_BOUNCE, true).setIterations(-1, LoopType.PingPong).setEaseType(EaseType.QuadOut));
+                indDisp = 0;
+                Go.to(this, 1.0f, new TweenConfig().floatProp("indDisp", INDICATOR_BOUNCE, true).setIterations(-1, LoopType.PingPong).setEaseType(EaseType.QuadOut));
             }
-            interactInd.x = (p.x - this.x) / 2;
+            interactInd.x = (p.x - this.x);
+            interactInd.y = (p.y - this.y) + INDICATOR_Y + indDisp;
             p.setInteractObject(this);
             return true;
         }
