f1b1f1d5bbe58e60119013af181e4764d32b56c4
Josh Maggard <jmaggard62@gmail.com>
2014-08-08 01:39:27 -0500

    new version number and figured out chest displacement

--- Assets/Resources/Scripts/C.cs
+++ Assets/Resources/Scripts/C.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class C
 {
-    public const string versionNumber = "v0.2";
+    public const string versionNumber = "v0.21";
 
     public static float sceneTransitionTime = 1.5f;
     public static bool isSpawning = false;
